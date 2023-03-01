05984c8c2f8cbd1129953bca2b94462131036a6a
Koen De Groot <koendegroot84@gmail.com>
2023-03-01 21:35:21 +0100

    Added other form inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +38,10 @@ import { MatRadioModule } from '@angular/material/radio';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatRadioModule
+    MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-registration/create-registration.component.ts
+++ src/app/create-registration/create-registration.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-registration.component.scss']
 })
 export class CreateRegistrationComponent implements OnInit {
-
+  public packages = ['Monthly', 'Quarterly', 'Yearly']
+  public importantList = ["Toxic fat reduction", "Energy and endurance", "building lean muscle",
+    "Healthier digestive system", "Sugar craving body", "Fitness"]
   constructor() { }
 
   ngOnInit(): void {
