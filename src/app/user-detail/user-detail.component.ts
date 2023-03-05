464ce67101f54d6675a5dcc6b5f4694f6036de07
Koen De Groot <koendegroot84@gmail.com>
2023-03-05 19:11:51 +0100

    Added details page bindings

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../models/user.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(value => {
+      this.userId = value['id'];
+      this.fetchUserDetails(this.userId);
+    })
   }
 
+  public userId!: number;
+  userDetail!: User;
+
+  fetchUserDetails(userId: number) {
+    this.api.GetRegisteredUser(userId).subscribe(res => {
+      this.userDetail = res;
+    });
+  }
 }
